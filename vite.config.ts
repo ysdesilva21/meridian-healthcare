5252c7072a1d3668625ca96690f0efad8903034a
Sanju De Silva <yohanidesilva.dev.21@gmail.com>
2026-08-18 18:22:42 +0530

    change vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
-
-    visualizer({
-      filename: "./dist/stats.html",
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-    }),
+    tailwindcss(),
   ],
 });
